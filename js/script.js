ce318d698590e6f023207cba5556779e1d893da3
Karsh <akarsh16reddy@gmail.com>
2022-03-05 18:26:42 +0530

    Message is displayed in web page instead of alert

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ document.addEventListener(
 	"DOMContentLoaded",
 	function(event)
 	{
-		alert('The page is loaded and the script ran!')
+		var message = document.getElementById("message")
+		message.innerText = 'The script ran!'
 	}
 )
